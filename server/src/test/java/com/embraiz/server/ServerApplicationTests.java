cf399a3b1ed66e43e7920ba79e8fd43597ece958
lengqing66 <524872828@qq.com>
2020-09-17 08:56:30 +0800

    0917

--- server/src/test/java/com/embraiz/server/ServerApplicationTests.java
+++ server/src/test/java/com/embraiz/server/ServerApplicationTests.java
@@ -3,11 +3,27 @@ package com.embraiz.server;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import java.util.HashMap;
+import java.util.List;
+
 @SpringBootTest
 class ServerApplicationTests {
+    @persistenContext
+    private EntityManager em;
 
     @Test
-    void contextLoads() {
+    void AutoCreate() {
+        Query query = em.createNativeQuery("select xxxxx");
+        List<HashMap> map  = ((Query) query).getResultList();
+        StringBuffer output = new StringBuffer();
+        for(xxxx){
+            if(map[0]=="varchar"){
+                output.append("@Column(name....,.");
+            }
+        }
+        System.out.println(output);
     }
 
 }
